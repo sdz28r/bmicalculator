be41b0bc0afbfba205473fb068fdedb6ca2b3352
sdz28r <siddhant2807r@gmail.com>
2020-04-21 04:49:32 +0530

    second-commit

--- script.js
+++ script.js
@@ -280,10 +280,11 @@ function selectOnlyThisMetric(id) {
 }
 
 function iframeLoaded() {
-      var iFrameID = document.getElementById('idIframe');
-      if(iFrameID) {
-            // here you can make the height, I delete it first, then I make it again
-            iFrameID.height = "";
-            iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
-      }   
-  }
+    var iFrameID = document.getElementById('idIframe');
+    if(iFrameID) {
+        // here you can make the height, I delete it first, then I make it again
+        iFrameID.height = "";
+        iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+    }   
+}
+  
